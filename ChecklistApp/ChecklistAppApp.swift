bef2b69576f5030ea1e692cc633ad6e62d33b12d
Oliver Pinder <oliverpinder@me.com>
2025-05-30 15:57:52 +0100

    Initial Commit

---
+++ ChecklistApp/ChecklistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistAppApp.swift
+//  ChecklistApp
+//
+//  Created by Oliver Pinder on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChecklistAppTests/ChecklistAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistAppTests.swift
+//  ChecklistAppTests
+//
+//  Created by Oliver Pinder on 30/05/2025.
+//
+
+import Testing
+@testable import ChecklistApp
+
+struct ChecklistAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
