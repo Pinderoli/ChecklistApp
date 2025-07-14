0600c08dbbc509ace8daf8d47e97fdf0f7cb26ae
Oliver Pinder <oliverpinder@me.com>
2025-07-14 18:23:25 +0100

    - Created new file to hold tabbed lists

---
+++ ChecklistApp/CreateTabbedListView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTabbedListView.swift
+//  ChecklistApp
+//
+//  Created by Oliver Pinder on 14/07/2025.
+//
+
+import SwiftUI
+
+struct CreateTabbedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTabbedListView()
+}
